ff6a80688a501d61756c5069f6bbddde8f8546ee
jamjen831 <jamesonjendreas@gmail.com>
2021-04-04 00:09:20 -0700

    Update Get-Weather.psm1

--- Get-Weather.psm1
+++ Get-Weather.psm1
@@ -1,15 +1,16 @@
 ﻿ function Get-Weather {
     #Set City, API Key and units
+    #Create the query URL for our weather Data
     $cityID = "yourCityID" 
     $apiKey =  "yourAPIKey" 
     $units = "imperial"
     $callURL = "http://api.openweathermap.org/data/2.5/weather?id=$cityID&units=$units&appid=$apiKey"      
     
-    #get the data and convert
+    #get the data, process the raw data as JSON, then turn it into an object
     $initalJson = Invoke-RestMethod -UseBasicParsing "$callURL"
     $weatherData = ConvertTo-Json $initalJson | ConvertFrom-Json 
         
-    #set weather variables from the polled data
+    #set weather variables from the wather object
     $Lat = $weatherData.coord.lat
     $Lon = $weatherData.coord.lon
     $realTemp = $weatherData.main.temp
@@ -25,7 +26,8 @@
     $cldPerct = $weatherData.clouds.all
     $locName = $weatherData.name 
     
-    #Set Output, which variables to include and make the text file
+    #Set Output Path 
+    #Select which variables and static text to include and write the text file
     $outPath = "C:\Program Files (x86)\Blue Iris Tools\txt\temperature.txt"
     $weatherTxt = "$locName Temp:$realTemp F Humidity:$hum% Cloud:$cldPrct% Pressure:$pres hpa" | Out-File -FilePath $outPath -Force -Encoding ascii
   }
