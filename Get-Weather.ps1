8ce6cb114836f0597c3d46e596d464db78c1916b
jamjen831 <jamesonjendreas@gmail.com>
2021-04-04 00:06:12 -0700

    Update Get-Weather.ps1

--- Get-Weather.ps1
+++ Get-Weather.ps1
@@ -1,12 +1,20 @@
 ﻿#Place this and Get-Weather.psm1 Module into C:\Scripts or modify $modPath value to point to the module.
 #Run via Task Scheduler on a daily schedule re-ran every 5 minutes.
-#Import the Module from file
+
+#Set the Module (psm1) path
+#Import the Module
 $modPath = "C:\Scripts\Get-Weather.psm1"
 Import-Module $modPath
-#Set inital timer to zero and Polling time, in seconds
+
+#Set inital timer to zero
+#Set polling frequesncy time, in seconds
 $timer = 0
 $pollSec = 10
-#Check if the timer is under 4min 50sec, run the Get-Weather function if it is, and then add 10 seconds to the running timer and wait 10 seconds before we check again. 
+
+#Check if the timer is under 4min 50sec. 
+#Run the Get-Weather function if the is
+#Add 10 seconds to the running timer
+#wait 10 seconds before we check again. 
 While($timer -lt 290){
     Get-Weather
     $timer = ($timer + $pollSec)
