db7e1fcd0ec06e0130ff1e7432e5062ca81de084
JJ 831 <kdntendo@gmail.com>
2021-04-03 22:51:58 -0700

    modified:   Get-Weather.psm1 	Get-Weather-hmtl_old.psm1

--- Get-Weather.psm1
+++ Get-Weather.psm1
@@ -1,71 +1,32 @@
-﻿function Get-WeatherData {
-    $cityID = "Your City ID" 
-    $apiKey =  "YourAPI Key" 
-    $callURL = "http://api.openweathermap.org/data/2.5/weather?id=$cityID&units=imperial&mode=html&appid=$apiKey" 
-    $script:html = Invoke-RestMethod -UseBasicParsing "$callURL" 
-   } 
-
-  function ConvertHtml-ToText {
- #Credit to Winston (url below) with slight modifications
- #http://winstonfassett.com/blog/2010/09/21/html-to-text-conversion-in-powershell/
- param([System.String] $html)
-
- # remove line breaks, replace with spaces
- $html = $html -replace "(`r|`n|`t)", " "
-
- # remove invisible content
- @('head', 'style', 'script', 'object', 'embed', 'applet', 'noframes', 'noscript', 'noembed') | % {
-  $html = $html -replace "<$_[^>]*?>.*?</$_>", ""
- }
-
- # Condense extra whitespace
- $html = $html -replace "( )+", " "
- $html = $html -replace "More..", ""
-
-
- # Add line breaks
- @('div','p','blockquote','h[1-9]') | % { $html = $html -replace "</?$_[^>]*?>.*?</$_>", ("`n" + '$0' )} 
- # Add line breaks for self-closing tags
- @('div','p','blockquote','h[1-9]','br') | % { $html = $html -replace "<$_[^>]*?/>", ('$0' + "`n")} 
-
- #strip tags 
- $html = $html -replace "<[^>]*?>", ""
-  
- # replace common entities
- @( 
-  @("&amp;bull;", " * "),
-  @("&amp;lsaquo;", "<"),
-  @("&amp;rsaquo;", ">"),
-  @("&amp;(rsquo|lsquo);", "'"),
-  @("&amp;(quot|ldquo|rdquo);", '"'),
-  @("&amp;trade;", "(tm)"),
-  @("&amp;frasl;", "/"),
-  @("&amp;(quot|#34|#034|#x22);", '"'),
-  @('&amp;(amp|#38|#038|#x26);', "&amp;"),
-  @("&amp;(lt|#60|#060|#x3c);", "<"),
-  @("&amp;(gt|#62|#062|#x3e);", ">"),
-  @('&amp;(copy|#169);', "(c)"),
-  @("&amp;(reg|#174);", "(r)"),
-  @("&amp;nbsp;", " "),
-  @("&amp;(.{2,6});", "")
- ) | % { $html = $html -replace $_[0], $_[1] }
-
- #Remove degree symbol
- $html = $html -replace "°",""
- #remove extra lines
- $html = $html -replace '\n+', ""
- #remove spaces
- $html = $html -replace '\s+'," "
- return $html
-
-}
-
-function Write-WeatherText {
-  $outPath = "C:\Program Files (x86)\Blue Iris Tools\txt\temperature.txt"
-  $weatherTxt = ConvertHtml-ToText $html |
-  Out-File -FilePath $outPath -Force -Encoding ascii
+﻿ function Get-Weather {
+    #Set City, API Key and units
+    $cityID = "5393052" 
+    $apiKey =  "e28110e8c1c5f11fcb929def472de104" 
+    $units = "imperial"
+    $callURL = "http://api.openweathermap.org/data/2.5/weather?id=$cityID&units=$units&appid=$apiKey"      
+    
+    #get the data and convert
+    $initalJson = Invoke-RestMethod -UseBasicParsing "$callURL"
+    $weatherData = ConvertTo-Json $initalJson | ConvertFrom-Json | Out-Host
+    
+    #set weather variables from the polled data
+    $Lat = $weatherData.coord.lat
+    $Lon = $weatherData.coord.lon
+    $realTemp = $weatherData.main.temp
+    $feelTemp = $weatherData.main.feels_like
+    $maxTemp  = $weatherData.main.temp_max
+    $minTemp = $weatherData.main.temp_min
+    $pres = $weatherData.main.pressure
+    $hum = $weatherData.main.humidity
+    $vis = $weatherData.visibility
+    $wndSpd = $weatherData.wind.speed
+    $wndDeg = $weatherData.wind.deg
+    $wndGst = $weatherData.wind.gust
+    $cldPerct = $weatherData.clouds.all
+    $locName = $weatherData.name 
+    
+    #Set Output, which variables to include and make the text file
+    $outPath = "c:\temp\test.txt" #"C:\Program Files (x86)\Blue Iris Tools\txt\temperature.txt"
+    $weatherTxt = "$locName Temp:$realTempF Humidity:$hum% Cloud:$cldPrct% Pressure:$pres hpa" | Out-File -FilePath $outPath -Force -Encoding ascii
   }
-
-  function Get-Weather {
-    Get-WeatherData | ConvertHtml-ToText | Write-WeatherText
-    }
+  
